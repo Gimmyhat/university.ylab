441564ad6c87f90e22005380018d807389e69a07
Gimmyhat <nikitchenko.as@gmail.com>
2022-06-26 18:36:18 +0800

    homework-1

--- homework-1/3.py
+++ homework-1/3.py
@@ -3,7 +3,7 @@
 # возвращает количество конечных нулей в факториале (N! = 1 * 2 * 3 * ... * N) заданного числа:
 
 def zeros(n):
-    return sum(int(n / 5 ** i) for i in range(1, 10) if int(n / 5 ** i) >= 1)
+    return n / 5 + zeros(n / 5) if n >= 5 else 0
 
 
 assert zeros(0) == 0
